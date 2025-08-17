5fca96de789f638d6b27bb841256b3aaefe2225c
brianpr-pr <brian23perezperez.com>
2025-08-17 23:43:02 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php include "./includes/classes_handler.php";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Trackit</title>
+        <meta charset="UTF-8">
+        <link href="./style/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h2><a>Home</a></h2>
+            <h2><a>Diary</a></h2>
+            <h2><a>Books</a></h2>
+            <h2><a>Stats</a></h2>
+            <h2><a>Register</a></h2>
+            <h2><a>Log in</a></h2>
+            <h2><a>Account</a></h2>
+        </header>
+        <main>
+            <h1>Trackit allows you to set and achive study goals</h1>
+            <img id="graduation-img" src="./public/graduation.png">
+            <div id="first-review" class="review">
+                <img class="img-review" src="public/doctor.png">
+                <p class="paragraph-review">Thanks to Trackit I was able to finish my degree and i became a doctor,<br>
+            This was a life changer for me, this web allows me to keep improving my study sesions based on statistics that are real,<br>
+        Apart from not having to rely on feeling that im not doing enough.</p>
+            </div>
+            <div id="second-review" class="review">
+            <p class="paragraph-review">Thanks to Trackit I was able to finish my degree and i became a doctor,<br>
+            This was a life changer for me, this web allows me to keep improving my study sesions based on statistics that are real,<br>
+        Apart from not having to rely on feeling that im not doing enough.</p>
+                <img class="img-review" src="public/astronaut.png">
+            </div>
+            </div>
+        </main>
+
+        <footer>
+            <ul>
+                <li>Creator</li>
+                <li>About us</li>
+                <li>Sponsors</li>
+                <li>Visión</li>
+                <li>Job opportunities</li>
+            </ul>
+        </footer>
+    </body>
+</html>
