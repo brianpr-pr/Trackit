69a62b4b8c4dd66f4f685d312d6567a2578d1001
brianpr-pr <brian23perezperez.com>
2025-08-19 15:07:04 +0100

    Fixed issues with the implementation of the inclusion of the header

--- pages/stats.php
+++ pages/stats.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href=".././styles/styleGeneral.css">
-        <link rel="stylesheet" href=".././styles/styleStats.css">
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php include ".././includes/header.php";?>
-        <?php include ".././includes/footer.php";?>
-    </body>
-</html>
+<?php include "./../includes/classesHandler.php"?>
+<?php include "./../includes/head.php";?>
+<?php include "./../includes/footer.php";?>
